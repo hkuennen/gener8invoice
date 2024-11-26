74f6b7817f75419d4cb920f5a7895b05b9ae20c0
Hinnerk Künnen <hinnerk.kuennen@gmail.com>
2024-11-26 21:06:22 +0100

    got rid of table warnings

--- src/components/InvoiceSum.jsx
+++ src/components/InvoiceSum.jsx
@@ -2,45 +2,49 @@ const InvoiceSum = ({ subtotal, tax, handleTaxChange }) => {
   return (
     <>
       <table>
-        <tr className="grey">
-          <th className="th_pos w-5p"></th>
-          <th className="th_qty w-13p"></th>
-          <th className="th_item w-52p"></th>
-          <th className="th_price w-17p"></th>
-          <th className="th_amount w-17p"></th>
-        </tr>
-        <tr className="grey">
-          <td>Subtotal</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td id="subtotal">
-            € {isNaN(subtotal) ? (0).toFixed(2) : parseFloat(subtotal).toFixed(2)}
-          </td>
-        </tr>
-        <br />
-        <tr>
-          <td>Tax</td>
-          <td>
-            <select name="tax" onChange={(e) => handleTaxChange(e)}>
-              <option value="0.19">19 %</option>
-              <option value="0.07">7 %</option>
-            </select>
-          </td>
-          <td></td>
-          <td></td>
-          <td>€ {isNaN(subtotal) ? (0).toFixed(2) : (subtotal * parseFloat(tax)).toFixed(2)}</td>
-        </tr>
-        <br />
-        <tr className="bold">
-          <td>Total</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td id="total">
-            € {isNaN(subtotal) ? (0).toFixed(2) : (subtotal * (1 + parseFloat(tax))).toFixed(2)}
-          </td>
-        </tr>
+        <thead>
+          <tr className="bg-grey">
+            <th className="th_pos w-5p"></th>
+            <th className="th_qty w-13p"></th>
+            <th className="th_item w-52p"></th>
+            <th className="th_price w-17p"></th>
+            <th className="th_amount w-17p"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-grey">
+            <td>Subtotal</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td id="subtotal">
+              € {isNaN(subtotal) ? (0).toFixed(2) : parseFloat(subtotal).toFixed(2)}
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td>Tax</td>
+            <td>
+              <select name="tax" onChange={(e) => handleTaxChange(e)}>
+                <option value="0.19">19 %</option>
+                <option value="0.07">7 %</option>
+              </select>
+            </td>
+            <td></td>
+            <td></td>
+            <td>€ {isNaN(subtotal) ? (0).toFixed(2) : (subtotal * parseFloat(tax)).toFixed(2)}</td>
+          </tr>
+          <br />
+          <tr className="bold">
+            <td>Total</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td id="total">
+              € {isNaN(subtotal) ? (0).toFixed(2) : (subtotal * (1 + parseFloat(tax))).toFixed(2)}
+            </td>
+          </tr>
+        </tbody>
       </table>
     </>
   );
--- src/utils/TableSkeletons.js
+++ src/utils/TableSkeletons.js
@@ -3,13 +3,15 @@ import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 export const generateTableHeader = ({ isFirstPage }) => {
   return (
-    <tr className={isFirstPage ? "grey" : "no-bottom-border"}>
-      <th className="th_pos w-5p">{isFirstPage && "Pos"}</th>
-      <th className="th_qty w-13p">{isFirstPage && "Qty"}</th>
-      <th className="th_item w-52p">{isFirstPage && "Item"}</th>
-      <th className="th_price w-17p">{isFirstPage && "Unit price"}</th>
-      <th className="th_amount w-17p">{isFirstPage && "Amount"}</th>
-    </tr>
+    <thead>
+      <tr className={isFirstPage ? "bg-grey" : "no-bottom-border"}>
+        <th className="th_pos w-5p">{isFirstPage && "Pos"}</th>
+        <th className="th_qty w-13p">{isFirstPage && "Qty"}</th>
+        <th className="th_item w-52p">{isFirstPage && "Item"}</th>
+        <th className="th_price w-17p">{isFirstPage && "Unit price"}</th>
+        <th className="th_amount w-17p">{isFirstPage && "Amount"}</th>
+      </tr>
+    </thead>
   );
 };
 
@@ -22,58 +24,60 @@ export const generateTableBody = ({
   return (
     <>
       {positions.map((row, idx) => (
-        <tr key={idx + idxRows + 1}>
-          <td>{idx + idxRows + 1}</td>
-          <td>
-            <input
-              type="number"
-              name="qty"
-              placeholder="#"
-              className="td_qty w-50p"
-              value={row.qty}
-              onChange={(e) => handlePositionsChange(e, idx + idxRows)}
-              required
-            />
-          </td>
-          <td>
-            <input
-              type="text"
-              name="item"
-              className="td_item w-95p"
-              placeholder="Description of service or product..."
-              value={row.item}
-              onChange={(e) => handlePositionsChange(e, idx + idxRows)}
-              maxLength="55"
-              required
-            />
-          </td>
-          <td>
-            <div class="relative">
-              <p class="absolute left-1.5 text-slate-500">€ </p>
+        <tbody>
+          <tr key={idx + idxRows + 1}>
+            <td>{idx + idxRows + 1}</td>
+            <td>
               <input
                 type="number"
-                step="0.01"
-                name="price"
-                className="td_price w-80p"
-                placeholder="0.00"
-                value={row.price}
-                onFocus={(e) => (e.target.value = parseFloat(e.target.value).toFixed(2))}
-                onBlur={(e) => (e.target.value = parseFloat(e.target.value).toFixed(2))}
+                name="qty"
+                placeholder="#"
+                className="td_qty w-50p"
+                value={row.qty}
                 onChange={(e) => handlePositionsChange(e, idx + idxRows)}
                 required
               />
-            </div>
-          </td>
-          <td className="td_amount w-70p">
-            <label>€ </label>
-            {parseFloat(row.amount).toFixed(2)}
-          </td>
-          <td>
-            <button className="pointer" onClick={(e) => handleRemovePosition(e, idx + idxRows)}>
-              <FontAwesomeIcon icon={faTrashCan} />
-            </button>
-          </td>
-        </tr>
+            </td>
+            <td>
+              <input
+                type="text"
+                name="item"
+                className="td_item w-95p"
+                placeholder="Description of service or product..."
+                value={row.item}
+                onChange={(e) => handlePositionsChange(e, idx + idxRows)}
+                maxLength="55"
+                required
+              />
+            </td>
+            <td>
+              <div className="relative">
+                <p className="absolute left-1.5 text-slate-500">€ </p>
+                <input
+                  type="number"
+                  step="0.01"
+                  name="price"
+                  className="td_price w-80p"
+                  placeholder="0.00"
+                  value={row.price}
+                  onFocus={(e) => (e.target.value = parseFloat(e.target.value).toFixed(2))}
+                  onBlur={(e) => (e.target.value = parseFloat(e.target.value).toFixed(2))}
+                  onChange={(e) => handlePositionsChange(e, idx + idxRows)}
+                  required
+                />
+              </div>
+            </td>
+            <td className="td_amount w-70p">
+              <label>€ </label>
+              {parseFloat(row.amount).toFixed(2)}
+            </td>
+            <td>
+              <button className="pointer" onClick={(e) => handleRemovePosition(e, idx + idxRows)}>
+                <FontAwesomeIcon icon={faTrashCan} />
+              </button>
+            </td>
+          </tr>
+        </tbody>
       ))}
     </>
   );
